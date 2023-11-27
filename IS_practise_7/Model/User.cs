1bf1b3d79a8188cc4c442aa60dbbe51dbd213b1d
rochaldo1 <xusainow.artur2018@yandex.ru>
2023-11-27 12:59:56 +0700

    Добавьте файлы проекта.

---
+++ IS_practise_7/Model/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IS_practise_7.Model
+{
+    public class User
+    {
+        public string Login { get; private set; }
+        public string Password { get; private set; }
+        public Guid Id { get; private set; }
+        
+        public User()
+        {
+            
+        }
+    }
+}
