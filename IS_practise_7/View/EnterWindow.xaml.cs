6046ef734f6e7b6ca851e3b68a46e8e8f0731965
Dmitry <dima2323fr3@gmail.com>
2023-11-29 17:07:16 +0700

    Сделал файловую расстановку и начал иерархию

--- IS_practise_7/View/EnterWindow.xaml.cs
+++ IS_practise_7/View/EnterWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace IS_practise_7
+namespace IS_practise_7.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для EnterWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class EnterWindow : Window
     {
-        public MainWindow()
+        public EnterWindow()
         {
             InitializeComponent();
         }
