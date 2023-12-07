4311cbb253ee2f4caceb416fd1e1192989bcbedf
Dmitry <dima2323fr3@gmail.com>
2023-12-07 08:17:35 +0700

    Добавил окно ошибки

---
+++ IS_practise_7/View/ErrorWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IS_practise_7.View
+{
+    /// <summary>
+    /// Логика взаимодействия для ErrorWindow.xaml
+    /// </summary>
+    public partial class ErrorWindow : Window
+    {
+        public ErrorWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RollUp_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+        private void Exit_MouseDown(object sender, MouseButtonEventArgs e) => this.Close();
+    }
+}
